fa8365df7c2ef4777a0189c0eda6f59a268a1139
Natacha Key <nataliaklueva@hotmail.com>
2023-09-28 21:28:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
+          Hello, React
         </a>
       </header>
     </div>
