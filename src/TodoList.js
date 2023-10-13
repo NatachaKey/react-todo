dbec0a6799891ade9f3b5d67d6ef140b433086f6
Natacha Key <nataliaklueva@hotmail.com>
2023-10-13 23:30:53 +0200

    lesson 1_4

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
-const todoList = [
-  { id: 1, title: 'complete assignment' },
-  { id: 2, title: 'study SQL' },
-  { id: 3, title: 'watch Redux tutorial' },
-];
 
-function TodoList() {
+function TodoList({ todoList }) {
   return (
     <ul>
       {todoList.map(function (item) {
