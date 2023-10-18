d400c1373a8d5426fb9dc544d6042a89ce86c518
Natacha Key <nataliaklueva@hotmail.com>
2023-10-18 17:17:22 +0200

    Remove Custom Hook

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import React, { useState, useEffect } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-const useSemiPersistentState = () => {
+function App() {
   const [todoList, setTodoList] = useState(() => {
     const savedTodoList = localStorage.getItem('savedTodoList');
     return savedTodoList ? JSON.parse(savedTodoList) : []; //turn string we got with JSON.stringify back into an Array.
   });
+
   // Effect to save todoList to localStorage when it changes
   useEffect(() => {
     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
   }, [todoList]);
 
-  return [todoList, setTodoList];
-};
-
-function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
   }
   function removeTodo(id) {
-    const updatedTodoList = todoList.filter(item => item.id !== id);
+    const updatedTodoList = todoList.filter((item) => item.id !== id);
     setTodoList(updatedTodoList);
   }
 
@@ -30,7 +26,7 @@ function App() {
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
 
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo}  />
+      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
     </>
   );
 }
