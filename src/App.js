8e6af097d0548d2345bd992b0255a7bbe3af19a6
Natacha Key <nataliaklueva@hotmail.com>
2023-10-18 22:58:22 +0200

    comments added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TodoList from './TodoList';
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+//useEffect for Initial Load , async fetching a todo list from localStorage. it updates the todoList state and sets isLoading to false.
   useEffect(() => {
     new Promise((resolve, reject) =>
       setTimeout(
@@ -23,7 +23,7 @@ function App() {
       setIsLoading(false);
     });
   }, []);
-
+//useEffect for Saving to localStorage: watches for changes in isLoading and todoList, When isLoading is false, it saves the current todoList to localStorage.
   useEffect(() => {
     if (!isLoading) {
       localStorage.setItem('savedTodoList', JSON.stringify(todoList));
