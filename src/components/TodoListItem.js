53d9b6b4cc4fd1878e532f9eff060b2e0fccff86
Natacha Key <nataliaklueva@hotmail.com>
2024-01-30 11:18:07 +0100

    added PropTypes to another components

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './TodoListItem.module.css';
+import PropTypes from 'prop-types';
 
 function TodoListItem({ todo, onRemoveTodo }) {
   const handleRemoveClick = () => {
@@ -7,11 +8,23 @@ function TodoListItem({ todo, onRemoveTodo }) {
   };
   return (
     <div>
-      <li className = {style.listItem}>{todo.title}</li>
+      <li className={style.listItem}>{todo.title}</li>
       <li>Created time: {todo.createdTime}</li>
-      <button className= {style.btn} onClick={handleRemoveClick}>Remove</button>
+      <button className={style.btn} onClick={handleRemoveClick}>
+        Remove
+      </button>
     </div>
   );
 }
+
+TodoListItem.propTypes = {
+  // shape - expected object structure
+  todo: PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string,
+    createdTime: ProtoTypes.string,
+  }),
+  onRemoveTodo: PropTypes.func,
+};
 
 export default TodoListItem;
